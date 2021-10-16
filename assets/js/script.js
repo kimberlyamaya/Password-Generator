2f299c62ef6e45fa180039b42077ae209841a9b7
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-10-15 19:34:53 -0700

    setup of file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 // Assignment code here
 
-function getRandomLower () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
 
-console.log(getRandomLower());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
